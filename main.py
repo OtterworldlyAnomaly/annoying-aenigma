b260ecf299acecaf528a26f22266596d2cc3a46c
OtterworldlyAnomaly <marc.pereira@outlook.de>
2026-03-31 14:56:05 +0200

    changing direction

--- main.py
+++ main.py
@@ -5,17 +5,25 @@ import toml
 # libraries imports
 
 
-q_and_a = dict()
+sample_dict = {"Paul": "Phoenix",
+           "Asuka": "Kazama",
+           "Jin": "Kazama",
+           "Jun": "Kazama",
+           "Kazuya": "Mishima",
+           "Heihachi": "Mishima",
+           "Steve": "Fox",
+           "Miary": "Zo",
+           }
 # Dictionaries for questions and answers
 
-rand = list(range(0,len(q_and_a.items()))) # list of integers from 0 to 9
+rand = list(range(0,len(sample_dict.items()))) # list of integers from 0 to 9
 random.shuffle(rand)
 count = 0
 # random number generator to shuffle q and a
 
-def add_to_qna():
-    question = input("Enter a question"); answer = input("Enter the answer")
-    q_and_a[question] = answer
+def add_to_dict():
+    first_v = input("Enter first value"); second_v = input("Enter second value")
+    sample_dict[first_v] = second_v
 
 # Create the main application window
 root = tk.Tk()
@@ -27,8 +35,12 @@ label = tk.Label(root, text="test", font=("Arial", 12))
 label.pack(pady=5)
 
 # Create and place a button
-greet_button = tk.Button(root, text="Next", font=("Arial", 12),command=q_and_a)
-greet_button.pack(pady=10)
+# greet_button = tk.Button(root, text="Next", font=("Arial", 12))
+# greet_button.pack(pady=10)
+
+text = tk.Text(root)
+text.insert('1.0',sample_dict)
+text.pack(pady=4)
 
 # Start the Tkinter event loop
 if __name__ == "__main__":   
